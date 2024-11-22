8441bb0efbc8d0921b8af57b8d98dfa52bfd58c7
SaeedKarimian860 <SGK12190@gmail.com>
2024-11-22 00:59:10 -0500

    E-Commerce app

--- repositories/users.js
+++ repositories/users.js
@@ -30,6 +30,8 @@ class UsersRepository {
     records.push(attrs);
 
     await this.writeAll(records);
+
+    return attrs;
   }
 
   async writeAll(records) {
@@ -85,14 +87,4 @@ class UsersRepository {
   }
 }
 
-const test = async () => {
-  const repo = new UsersRepository("users.json");
-
-  const user = await repo.getOneBy({
-    password: "mypassword",
-  });
-
-  console.log(user);
-};
-
-test();
+module.exports = new UsersRepository("users.json");
