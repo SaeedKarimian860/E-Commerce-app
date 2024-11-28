e2c66eeeb6ecda1c16d84842927cb832cbbe5efb
SaeedKarimian860 <SGK12190@gmail.com>
2024-11-28 11:24:05 -0500

    E-Commerce app

--- routes/admin/auth.js
+++ routes/admin/auth.js
@@ -18,26 +18,26 @@ router.post(
       .trim()
       .normalizeEmail()
       .isEmail()
-      .withMessage('Must be a valid email')
+      .withMessage("Must be a valid email")
       .custom(async (email) => {
         const existingUser = await usersRepo.getOneBy({ email });
         if (existingUser) {
-          throw new Error ('Email in use');
+          throw new Error("Email in use");
         }
-      }), 
+      }),
     check("password")
       .trim()
       .isLength({ min: 4, max: 20 })
-      .withMessage('Must be between 4 and 20 characters'), 
+      .withMessage("Must be between 4 and 20 characters"),
     check("passwordConfirmation")
       .trim()
       .isLength({ min: 4, max: 20 })
-      .withMessage('Must be between 4 and 20 characters')
+      .withMessage("Must be between 4 and 20 characters")
       .custom((passwordConfirmation, { req }) => {
         if (passwordConfirmation !== req.body.password) {
-          throw new Error ('Passwords must match');
+          throw new Error("Passwords must match");
         }
-      })
+      }),
   ],
   async (req, res) => {
     const errors = validationResult(req);
@@ -45,7 +45,7 @@ router.post(
 
     const { email, password, passwordConfirmation } = req.body;
     const user = await usersRepo.create({ email, password });
-    
+
     req.session.userId = user.id;
 
     res.send("Account created!!");
